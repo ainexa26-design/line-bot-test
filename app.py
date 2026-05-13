82e7f9daaae407a54bc8747ac88eb529e4b6bcc8
福田虎之介 <fukudatoranosuke@fukudatoranosukenoMacBook-Air.local>
2026-05-13 13:54:47 +0900

    fix indent 2

--- app.py
+++ app.py
@@ -97,14 +97,14 @@ def handle_message(event):
             ai_message = "エラーが出ています"
 
             with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
+                line_bot_api = MessagingApi(api_client)
 
-        line_bot_api.reply_message(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=ai_message)]
-            )
-        )
+                line_bot_api.reply_message(
+                    ReplyMessageRequest(
+                        reply_token=event.reply_token,
+                        messages=[TextMessage(text=ai_message)]
+                    )
+                )
 
     
 
